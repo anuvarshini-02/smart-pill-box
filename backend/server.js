477aa43d65135f5f1ee2e87188c64d415822a5e4
anuvarshini-02 <vanuvarshini02@gmail.com>
2026-01-10 21:14:31 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,15 @@
 const express = require("express");
-const cors = require("cors");
-
 const app = express();
-const PORT = 3001;
 
-
-app.use(cors());
-app.use(express.json());
-
-// test route
 app.get("/", (req, res) => {
-  res.send("Smart Pill Box Backend Running");
+  res.send(`
+    <h1>Smart Pill Box</h1>
+    <p>Backend is running successfully 🚀</p>
+    <p>Status: Active</p>
+  `);
 });
 
-// receive patient data
-app.post("/patient", (req, res) => {
-  const data = req.body;
-  console.log("Patient Data:", data);
-
-  res.json({
-    message: "Patient data received successfully",
-    data: data
-  });
-});
-
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on port", PORT);
 });
-
